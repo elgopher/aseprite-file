bdebc767bd8b75155648c269154824328eff1208
Jacek Olszak <jacek.olszak@4finance.com>
2018-07-13 14:57:49 +0200

    Add license

--- src/main/java/com/github/jacekolszak/aseprite/impl/PaletteImpl.java
+++ src/main/java/com/github/jacekolszak/aseprite/impl/PaletteImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Aseprite File Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.jacekolszak.aseprite.impl;
 
 import java.util.HashMap;
@@ -11,19 +26,26 @@ import com.github.jacekolszak.aseprite.impl.ASE.Frame.Chunk.PaletteChunk.Palette
 public final class PaletteImpl implements Palette {
 
     private final Colors colors;
+    private final int transparentColorIndex;
 
     public PaletteImpl(ASE ase) {
         ASE.Frame frame = ase.frame(1); // TODO all those palette chunks are in the first frame only?
-        this.colors = frame.chunks()
+        colors = frame.chunks()
                 .stream()
                 .filter(Chunk::isPalette)
                 .map(Chunk::palette)
                 .collect(Colors::new, Colors::merge, Colors::merge);
+        transparentColorIndex = ase.header().transparentColorPaletteEntryIndex();
     }
 
     @Override
     public int numberOfColors() {
         return colors.numberOfColors();
+    }
+
+    @Override
+    public int transparentColorIndex() {
+        return transparentColorIndex;
     }
 
     @Override
